7a1adb53084b3e7433d78f67cb7cbaace4498866
Vitor Rodrigues <Vitor.Rodrigues@samsys.pt>
2025-10-17 14:42:02 +0100

    fix url

--- app.js
+++ app.js
@@ -62,7 +62,7 @@
     // Register Service Worker
   if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
-          navigator.serviceWorker.register('/sw.js')
+          navigator.serviceWorker.register('sw.js')
               .then(registration => {
                   console.log('Service Worker registered successfully:', registration);
               })
