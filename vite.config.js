b425bbac62ab599600f28697f1fbf0f8eda653a8
Vitor Rodrigues <Vitor.Rodrigues@samsys.pt>
2025-10-28 11:33:32 +0000

    fix: set base path for deployment in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/demo-pwa-app/',  // Add this line!
+
     // Root directory (where index.html is)
     root: '.',
 
