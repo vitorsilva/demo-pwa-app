4fffaee64d3546074604ed0c09e6c259fb34ebfa
Vitor Silva <vitorsilva.com@gmail.com>
2026-01-13 12:47:55 +0000

    chore: update version number

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
 // Auto-generated - do not edit manually
-  export const APP_VERSION = '20260113.296';
-  export const BUILD_DATE = '2026-01-13T10:29:06.398Z';
+  export const APP_VERSION = '20260113.305';
+  export const BUILD_DATE = '2026-01-13T12:46:57.379Z';
   
